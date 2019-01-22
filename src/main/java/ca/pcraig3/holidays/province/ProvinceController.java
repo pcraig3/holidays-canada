73510d3687224194da1654278b092131fb44439e
Paul Craig <paul_craig_16@hotmail.com>
2019-01-21 23:56:00 -0500

    Return a resource list for provinces

--- src/main/java/ca/pcraig3/holidays/province/ProvinceController.java
+++ src/main/java/ca/pcraig3/holidays/province/ProvinceController.java
@@ -2,6 +2,7 @@ package ca.pcraig3.holidays.province;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -27,11 +29,15 @@ public class ProvinceController {
     }
 
     @GetMapping
-    List<Province> all() {
+    Resources<Resource<Province>> all() {
         log.info("Get all '/provinces/'");
+        
+        List<Resource<Province>> provinces = repository.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
 
-
-        return this.repository.findAll();
+        return new Resources<>(provinces,
+                linkTo(methodOn(ProvinceController.class).all()).withSelfRel());
     }
 
     @GetMapping("/{id}")
